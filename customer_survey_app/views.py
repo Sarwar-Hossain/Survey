df5ad7b15490e66a6626255cde70271a5ebb0f96
Sarwar Hossain <msarwar98@gmail.com>
2022-03-10 15:31:37 +0600

    Work: Code Optimized. Instead of ORM used raw Query.

--- customer_survey_app/views.py
+++ customer_survey_app/views.py
@@ -4,6 +4,9 @@ from django.shortcuts import render, redirect
 from customer_survey_app.models import *
 from django.contrib import messages
 from customer_survey_app.aes_cipher import AESCipher
+from django.db import connection
+
+cursor = connection.cursor()
 
 
 def login(request):
@@ -53,163 +56,125 @@ def login(request):
 def customer_survey_report(request):
     try:
         if request.session.has_key('user_name'):
-            feedback = CustomerFeedback.objects.all()
 
             # Question 1
-            question_1_true_part = 0
-            question_1_false_part = 0
-            question_1_whole = 0
+            cursor.execute('''SELECT 
+            sum(CASE WHEN csac.question_1 IS true THEN 1 ELSE 0 END) AS question_1_true_part,
+            sum(CASE WHEN  csac.question_1 IS false THEN 1 ELSE 0 END) AS question_1_false_part,
+            sum(CASE WHEN  csac.question_1 IS false THEN 1 ELSE 1 END) AS question_1_whole
+            FROM customer_survey_app_customerfeedback csac;''')
 
-            for dt in feedback:
-                if dt.question_1 is True:
-                    question_1_true_part = question_1_true_part + 1
-                    question_1_whole = question_1_whole + 1
-                elif dt.question_1 is False:
-                    question_1_false_part = question_1_false_part + 1
-                    question_1_whole = question_1_whole + 1
-                else:
-                    question_1_whole = question_1_whole + 1
+            result = cursor.fetchall()
 
-            question_1_true = get_percentage(question_1_true_part, question_1_whole)
-            question_1_false = get_percentage(question_1_false_part, question_1_whole)
+            row_headers = [col[0] for col in cursor.description]
+            data = [dict(zip(row_headers, row)) for row in result]
+
+            question_1_true = get_percentage(data[0]['question_1_true_part'], data[0]['question_1_whole'])
+            question_1_false = get_percentage(data[0]['question_1_false_part'], data[0]['question_1_whole'])
 
             # Question 2
-            question_2_friendly_part = 0
-            question_2_knowledgeable_part = 0
-            question_2_helpful_part = 0
-            question_2_whole = 0
+            cursor.execute(''' SELECT 
+            sum(CASE WHEN csac.question_2 = 'Friendly' THEN 1 ELSE 0 END) AS question_2_friendly_part,
+            sum(CASE WHEN csac.question_2 = 'Knowledgeable' THEN 1 ELSE 0 END) AS question_2_knowledgeable_part,
+            sum(CASE WHEN csac.question_2 = 'Helpful' THEN 1 ELSE 0 END) AS question_2_helpful_part,
+            sum(CASE WHEN csac.question_2 = 'Helpful' THEN 1 ELSE 1 END) AS question_2_whole
+            FROM customer_survey_app_customerfeedback csac; ''')
 
-            for dt in feedback:
-                if dt.question_2 == 'Friendly':
-                    question_2_friendly_part = question_2_friendly_part + 1
-                    question_2_whole = question_2_whole + 1
-                elif dt.question_2 == 'Knowledgeable':
-                    question_2_knowledgeable_part = question_2_knowledgeable_part + 1
-                    question_2_whole = question_2_whole + 1
-                elif dt.question_2 == 'Helpful':
-                    question_2_helpful_part = question_2_helpful_part + 1
-                    question_2_whole = question_2_whole + 1
-                else:
-                    question_2_whole = question_2_whole + 1
+            result = cursor.fetchall()
 
-            question_2_friendly = get_percentage(question_2_friendly_part, question_2_whole)
-            question_2_knowledgeable = get_percentage(question_2_knowledgeable_part, question_2_whole)
-            question_2_helpful = get_percentage(question_2_helpful_part, question_2_whole)
+            row_headers = [col[0] for col in cursor.description]
+            data = [dict(zip(row_headers, row)) for row in result]
+
+            question_2_friendly = get_percentage(data[0]['question_2_friendly_part'], data[0]['question_2_whole'])
+            question_2_knowledgeable = get_percentage(data[0]['question_2_knowledgeable_part'],
+                                                      data[0]['question_2_whole'])
+            question_2_helpful = get_percentage(data[0]['question_2_helpful_part'], data[0]['question_2_whole'])
 
             # Question 3
-            question_3_slightly_part = 0
-            question_3_moderate_part = 0
-            question_3_strongly_part = 0
-            question_3_whole = 0
+            cursor.execute(''' SELECT 
+            sum(CASE WHEN csac.question_3 = 'Slightly' THEN 1 ELSE 0 END) AS question_3_slightly_part,
+            sum(CASE WHEN csac.question_3 = 'Moderate' THEN 1 ELSE 0 END) AS question_3_moderate_part,
+            sum(CASE WHEN csac.question_3 = 'Strongly' THEN 1 ELSE 0 END) AS question_3_strongly_part,
+            sum(CASE WHEN csac.question_3 = 'Strongly' THEN 1 ELSE 1 END) AS question_3_whole
+            FROM customer_survey_app_customerfeedback csac; ''')
 
-            for dt in feedback:
-                if dt.question_3 == 'Slightly':
-                    question_3_slightly_part = question_3_slightly_part + 1
-                    question_3_whole = question_3_whole + 1
-                elif dt.question_3 == 'Moderate':
-                    question_3_moderate_part = question_3_moderate_part + 1
-                    question_3_whole = question_3_whole + 1
-                elif dt.question_3 == 'Strongly':
-                    question_3_strongly_part = question_3_strongly_part + 1
-                    question_3_whole = question_3_whole + 1
-                else:
-                    question_3_whole = question_3_whole + 1
+            result = cursor.fetchall()
 
-            question_3_slightly = get_percentage(question_3_slightly_part, question_3_whole)
-            question_3_moderate = get_percentage(question_3_moderate_part, question_3_whole)
-            question_3_strongly = get_percentage(question_3_strongly_part, question_3_whole)
+            row_headers = [col[0] for col in cursor.description]
+            data = [dict(zip(row_headers, row)) for row in result]
+
+            question_3_slightly = get_percentage(data[0]['question_3_slightly_part'], data[0]['question_3_whole'])
+            question_3_moderate = get_percentage(data[0]['question_3_moderate_part'], data[0]['question_3_whole'])
+            question_3_strongly = get_percentage(data[0]['question_3_strongly_part'], data[0]['question_3_whole'])
 
             # Question 4
-            question_4_true_part = 0
-            question_4_false_part = 0
-            question_4_whole = 0
+            cursor.execute(''' SELECT 
+            sum(CASE WHEN csac.question_4 IS true THEN 1 ELSE 0 END) AS question_4_true_part,
+            sum(CASE WHEN  csac.question_4 IS false THEN 1 ELSE 0 END) AS question_4_false_part,
+            sum(CASE WHEN  csac.question_4 IS false THEN 1 ELSE 1 END) AS question_4_whole
+            FROM customer_survey_app_customerfeedback csac; ''')
 
-            for dt in feedback:
-                if dt.question_4 is True:
-                    question_4_true_part = question_4_true_part + 1
-                    question_4_whole = question_4_whole + 1
-                elif dt.question_4 is False:
-                    question_4_false_part = question_4_false_part + 1
-                    question_4_whole = question_4_whole + 1
-                else:
-                    question_4_whole = question_4_whole + 1
+            result = cursor.fetchall()
 
-            question_4_true = get_percentage(question_4_true_part, question_4_whole)
-            question_4_false = get_percentage(question_4_false_part, question_4_whole)
+            row_headers = [col[0] for col in cursor.description]
+            data = [dict(zip(row_headers, row)) for row in result]
+
+            question_4_true = get_percentage(data[0]['question_4_true_part'], data[0]['question_4_whole'])
+            question_4_false = get_percentage(data[0]['question_4_false_part'], data[0]['question_4_whole'])
 
             # Question 5
-            question_5_true_part = 0
-            question_5_false_part = 0
-            question_5_whole = 0
+            cursor.execute(''' SELECT 
+            sum(CASE WHEN csac.question_5 IS true THEN 1 ELSE 0 END) AS question_5_true_part,
+            sum(CASE WHEN  csac.question_5 IS false THEN 1 ELSE 0 END) AS question_5_false_part,
+            sum(CASE WHEN  csac.question_5 IS false THEN 1 ELSE 1 END) AS question_5_whole
+            FROM customer_survey_app_customerfeedback csac; ''')
 
-            for dt in feedback:
-                if dt.question_5 is True:
-                    question_5_true_part = question_5_true_part + 1
-                    question_5_whole = question_5_whole + 1
-                elif dt.question_5 is False:
-                    question_5_false_part = question_5_false_part + 1
-                    question_5_whole = question_5_whole + 1
-                else:
-                    question_5_whole = question_5_whole + 1
+            result = cursor.fetchall()
 
-            question_5_true = get_percentage(question_5_true_part, question_5_whole)
-            question_5_false = get_percentage(question_5_false_part, question_5_whole)
+            row_headers = [col[0] for col in cursor.description]
+            data = [dict(zip(row_headers, row)) for row in result]
+
+            question_5_true = get_percentage(data[0]['question_5_true_part'], data[0]['question_5_whole'])
+            question_5_false = get_percentage(data[0]['question_5_false_part'], data[0]['question_5_whole'])
 
             # Question 6
-            question_6_small_part = 0
-            question_6_medium_part = 0
-            question_6_large_part = 0
-            question_6_extra_large_part = 0
-            question_6_extra_extra_large_part = 0
-            question_6_whole = 0
+            cursor.execute(''' SELECT 
+            sum(CASE WHEN csac.question_6 = 'Small' THEN 1 ELSE 0 END) AS question_6_small_part,
+            sum(CASE WHEN csac.question_6 = 'Medium' THEN 1 ELSE 0 END) AS question_6_medium_part,
+            sum(CASE WHEN csac.question_6 = 'Large' THEN 1 ELSE 0 END) AS question_6_large_part,
+            sum(CASE WHEN csac.question_6 = 'Extra Large' THEN 1 ELSE 0 END) AS question_6_extra_large_part,
+            sum(CASE WHEN csac.question_6 = 'Extra Extra Large' THEN 1 ELSE 0 END) AS question_6_extra_extra_large_part,
+            sum(CASE WHEN csac.question_6 = 'Small' THEN 1 ELSE 1 END) AS question_6_whole
+            FROM customer_survey_app_customerfeedback csac; ''')
 
-            for dt in feedback:
-                if dt.question_6 == 'Small':
-                    question_6_small_part = question_6_small_part + 1
-                    question_6_whole = question_6_whole + 1
-                elif dt.question_6 == 'Medium':
-                    question_6_medium_part = question_6_medium_part + 1
-                    question_6_whole = question_6_whole + 1
-                elif dt.question_6 == 'Large':
-                    question_6_large_part = question_6_large_part + 1
-                    question_6_whole = question_6_whole + 1
-                elif dt.question_6 == 'Extra Large':
-                    question_6_extra_large_part = question_6_extra_large_part + 1
-                    question_6_whole = question_6_whole + 1
-                elif dt.question_6 == 'Extra Extra Large':
-                    question_6_extra_extra_large_part = question_6_extra_extra_large_part + 1
-                    question_6_whole = question_6_whole + 1
-                else:
-                    question_6_whole = question_6_whole + 1
+            result = cursor.fetchall()
 
-            question_6_small = get_percentage(question_6_small_part, question_6_whole)
-            question_6_medium = get_percentage(question_6_medium_part, question_6_whole)
-            question_6_large = get_percentage(question_6_large_part, question_6_whole)
-            question_6_extra_large = get_percentage(question_6_extra_large_part, question_6_whole)
-            question_6_extra_extra_large = get_percentage(question_6_extra_extra_large_part, question_6_whole)
+            row_headers = [col[0] for col in cursor.description]
+            data = [dict(zip(row_headers, row)) for row in result]
+
+            question_6_small = get_percentage(data[0]['question_6_small_part'], data[0]['question_6_whole'])
+            question_6_medium = get_percentage(data[0]['question_6_medium_part'], data[0]['question_6_whole'])
+            question_6_large = get_percentage(data[0]['question_6_large_part'], data[0]['question_6_whole'])
+            question_6_extra_large = get_percentage(data[0]['question_6_extra_large_part'], data[0]['question_6_whole'])
+            question_6_extra_extra_large = get_percentage(data[0]['question_6_extra_extra_large_part'],
+                                                          data[0]['question_6_whole'])
 
             # Question 7
-            question_7_black_part = 0
-            question_7_white_part = 0
-            question_7_others_part = 0
-            question_7_whole = 0
+            cursor.execute(''' SELECT 
+            sum(CASE WHEN csac.question_7 = 'Black' THEN 1 ELSE 0 END) AS question_7_black_part,
+            sum(CASE WHEN csac.question_7 = 'White' THEN 1 ELSE 0 END) AS question_7_white_part,
+            sum(CASE WHEN csac.question_7 = 'Others' THEN 1 ELSE 0 END) AS question_7_others_part,
+            sum(CASE WHEN csac.question_7 = 'Others' THEN 1 ELSE 1 END) AS question_7_whole
+            FROM customer_survey_app_customerfeedback csac; ''')
 
-            for dt in feedback:
-                if dt.question_7 == 'Black':
-                    question_7_black_part = question_7_black_part + 1
-                    question_7_whole = question_7_whole + 1
-                elif dt.question_7 == 'White':
-                    question_7_white_part = question_7_white_part + 1
-                    question_7_whole = question_7_whole + 1
-                elif dt.question_7 == 'Others':
-                    question_7_others_part = question_7_others_part + 1
-                    question_7_whole = question_7_whole + 1
-                else:
-                    question_7_whole = question_7_whole + 1
+            result = cursor.fetchall()
 
-            question_7_black = get_percentage(question_7_black_part, question_7_whole)
-            question_7_white = get_percentage(question_7_white_part, question_7_whole)
-            question_7_others = get_percentage(question_7_others_part, question_7_whole)
+            row_headers = [col[0] for col in cursor.description]
+            data = [dict(zip(row_headers, row)) for row in result]
+
+            question_7_black = get_percentage(data[0]['question_7_black_part'], data[0]['question_7_whole'])
+            question_7_white = get_percentage(data[0]['question_7_white_part'], data[0]['question_7_whole'])
+            question_7_others = get_percentage(data[0]['question_7_others_part'], data[0]['question_7_whole'])
 
             context = {
                 'question_1_true': question_1_true,
